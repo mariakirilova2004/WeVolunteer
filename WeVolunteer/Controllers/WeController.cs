7794a0d1aa7a2f83eb8b9d14e35f9c8b8b879c1a
Maria_Kirilova <mariakirilova2004@gmail.com>
2022-12-10 17:11:57 +0200

    Development About and Help sections

---
+++ WeVolunteer/Controllers/WeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeVolunteer.Controllers
+{
+    [AllowAnonymous]
+    public class WeController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+    }
+}
